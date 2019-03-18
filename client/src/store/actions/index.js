0cafd5fab8229bab566216239a61aa870142cdd5
RayB <raybernard1337@gmail.com>
2019-03-18 08:02:13 -0600

    Added developer profiles

--- client/src/store/actions/index.js
+++ client/src/store/actions/index.js
@@ -16,5 +16,6 @@ export {
   addExperience,
   addEducation,
   deleteExperience,
-  deleteEducation
+  deleteEducation,
+  getProfiles
 } from "./profileActions";
--- client/src/store/actions/profileActions.js
+++ client/src/store/actions/profileActions.js
@@ -110,6 +110,25 @@ export const deleteEducation = id => dispatch => {
     );
 };
 
+// Get all profiles
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get("/api/profile/all")
+    .then(res =>
+      dispatch({
+        type: actionTypes.GET_PROFILES,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: actionTypes.GET_PROFILES,
+        payload: null
+      })
+    );
+};
+
 // Delete account & profile
 export const deleteAccount = () => dispatch => {
   if (window.confirm("Are you sure? This can NOT be undone!")) {
